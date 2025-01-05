f14f003321ce6039c883729085c5f937d3818518
Imesha Madhumali <imeshamadhumali31@gmail.com>
2025-01-05 10:38:27 +0530

    Add SafeAreaProvider

--- App.js
+++ App.js
@@ -1,17 +1,28 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { PaperProvider } from 'react-native-paper';
-import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import { PaperProvider, Button, TextInput } from 'react-native-paper';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function App() {
+  const [text, setText] = useState('');
   return (
     <PaperProvider>
-      <SafeAreaView></SafeAreaView>
+      <SafeAreaProvider>
       <View style={styles.container}>
-      <Text variant="titleLarge" style={styles.title}>Curency Converter</Text>
+        <Text variant="titleLarge" style={styles.title}>Curency Converter</Text>
         <Text>Welcome to Convert Currency!</Text>
         <StatusBar style="auto" />
+        <TextInput
+          value={text}
+          onChangeText={text => setText(text)}
+        />
+        <Button mode="contained" onPress={() => console.log('Pressed')}>
+          Convert
+        </Button>
+        
       </View>
+      </SafeAreaProvider>
     </PaperProvider>
 
   );
