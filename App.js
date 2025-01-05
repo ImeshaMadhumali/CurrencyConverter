6b2a5555f74cddd7151bdd1cfc22901a2b71f0f0
Imesha Madhumali <imeshamadhumali31@gmail.com>
2025-01-05 08:53:40 +0530

    Install dependencies to web view

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { PaperProvider } from 'react-native-paper';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
     <PaperProvider>
+      <SafeAreaView></SafeAreaView>
       <View style={styles.container}>
       <Text variant="titleLarge" style={styles.title}>Curency Converter</Text>
         <Text>Welcome to Convert Currency!</Text>
